0bed411cc60bf42e73883a263d0f6ab0966570de
Duke <agueroduc01@gmail.com>
2022-06-03 23:11:12 +0700

    updateChangePass

--- src/main/java/fashion_shop/controller/TestController.java
+++ src/main/java/fashion_shop/controller/TestController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fashion_shop.DAO.productDAO;
-import fashion_shop.entity.Name;
 
 
 @Controller
